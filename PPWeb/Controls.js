787936fc776c47ad400743e8c0ad1bb2ef965985
gandeldev <gandel.dev@gmail.com>
2023-07-14 18:47:43 -0400

    Pin pong web

---
+++ PPWeb/Controls.js
@@ -0,0 +1,9 @@
+const playerOne = document.querySelector('.player.one')
+const playerTwo = document.querySelector('.player.two')
+
+function movePlayerOne() {
+    if (event.keyCode == 87) {
+        playerOne.style = height: ;
+        playerOne.getPosition
+    }
+}
